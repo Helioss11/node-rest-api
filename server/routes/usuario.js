cbf5405106b420269dba9022a1a02f4db47579d4
Helios Figueroa <helios.figueroa@azlo.com>
2020-06-16 18:44:43 -0500

    adding users schema

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const app = express();
 
- app.get('/usuario', (req, res) => {
-    res.json('get Hello world');
-  });
- 
-  app.post('/usuario', (req, res) => {
-   res.json('post Hello world');
- });
- 
- app.put('/usuario/:id', (req, res) => {
-   let id = req.params.id;
-   res.json({
-     id,
-     ...req.body
-   });
- });
- 
- app.delete('/usuario/:id', (req, res) => {
-   res.json('delete Hello world');
- });
+app.get('/usuario', (req, res) => {
+  res.json('get Hello world');
+});
 
- module.exports = app;
+app.post('/usuario', (req, res) => {
+  res.json('post Hello world');
+});
+ 
+app.put('/usuario/:id', (req, res) => {
+  let id = req.params.id;
+  res.json({
+    id,
+    ...req.body
+  });
+});
+ 
+app.delete('/usuario/:id', (req, res) => {
+  res.json('delete Hello world');
+});
+
+module.exports = app;
